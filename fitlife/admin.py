779db9ea123d388d51b8cb0fa2d2fa3fa3851ce4
Maria Luiza Morais <marialuiza2002moraes@gmail.com>
2026-06-26 20:17:12 -0300

    update: admin.py
    Atualização das funções do admin do projeto.

--- fitlife/admin.py
+++ fitlife/admin.py
@@ -1,1 +1,13 @@
 from django.contrib import admin
+from .models import Aluno, Professor, Plano, ContratoPlan, Pagamento, Fatura, Modalidade, Turma, Inscricao, Unidade
+# Register your models here.
+admin.site.register(Aluno)
+admin.site.register(Professor)
+admin.site.register(Plano)
+admin.site.register(ContratoPlan)
+admin.site.register(Pagamento)
+admin.site.register(Fatura)
+admin.site.register(Modalidade)
+admin.site.register(Turma)
+admin.site.register(Inscricao)
+admin.site.register(Unidade)
